86cccc5a62b6d0e5f0d08346489393c0a579ded8
Ton Soares <everton.soares@elevenfinancial.com>
2022-01-29 18:06:50 -0300

    Aula 03

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "38cd494f1fa6e58d"; }
+/******/ 		__webpack_require__.h = function() { return "c343aab044cabfce"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import React from 'react'
+import { useRouter } from 'next/router'
 import appConfig from './config.json'
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  )
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1'
@@ -62,11 +33,12 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'tonsn9'
+  // const username = 'tonsn9'
+  const [username, setUsername] = React.useState('tonsn9')
+  const roteamneto = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex',
@@ -74,7 +46,7 @@ export default function PaginaInicial() {
           justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
           backgroundImage:
-            'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            'url(https://virtualbackgrounds.site/wp-content/uploads/2020/12/bright-gaming-room-setup.jpg)',
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'cover',
           backgroundBlendMode: 'multiply'
@@ -90,8 +62,8 @@ export default function PaginaInicial() {
               sm: 'row'
             },
             width: '100%',
-            maxWidth: '700px',
-            borderRadius: '5px',
+            maxWidth: '600px',
+            borderRadius: '5px .',
             padding: '32px',
             margin: '16px',
             boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
@@ -101,6 +73,11 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault()
+              // window.location.href = '/chat'
+              roteamneto.push('/chat')
+            }}
             styleSheet={{
               display: 'flex',
               flexDirection: 'column',
@@ -111,7 +88,7 @@ export default function PaginaInicial() {
               marginBottom: '32px'
             }}
           >
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
+            <Titulo tag="h2">Bora bater um papo!</Titulo>
             <Text
               variant="body3"
               styleSheet={{
@@ -122,7 +99,27 @@ export default function PaginaInicial() {
               {appConfig.name}
             </Text>
 
+            {/* <input
+              type="text"
+              value={username}
+              onChange={function handler(event) {
+                // Onde esta o valor?
+                const valor = event.target.value
+                //Trocar o valor da variável
+                //através do React e avise quem precisa
+                setUsername(valor)
+              }}
+            /> */}
+
             <TextField
+              value={username}
+              onChange={function handler(event) {
+                // Onde esta o valor?
+                const valor = event.target.value
+                //Trocar o valor da variável
+                //através do React e avise quem precisa
+                setUsername(valor)
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
